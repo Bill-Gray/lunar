ec3ae920a1777d37e610cca259f7f93dd87979e8
Bill-Gray <pluto@projectpluto.com>
2018-03-03 21:35:11 -0500

    Added GPL notice to .cpp files that lacked one

--- htc20b.cpp
+++ htc20b.cpp
@@ -1,3 +1,20 @@
+/* Copyright (C) 2018, Project Pluto
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+02110-1301, USA. */
+
 /* -----------------------------------------------------------------------! */
 /* Downloaded from ftp://ftp.imcce.fr/pub/ephem/satel/htc20/htc20.f         */
 /* translated by f2c (version 20100827),  then heavily edited.              */
