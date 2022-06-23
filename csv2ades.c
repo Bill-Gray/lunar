348c7571840ec03d909b59a9b4c69dacf913c11f
Bill-Gray <pluto@projectpluto.com>
2022-06-23 17:15:40 -0400

    Updated code to convert Gaia .csv files to ADES to use DR3 (previous was hard-wired to DR2;  can now use either)

--- csv2ades.c
+++ csv2ades.c
@@ -2,6 +2,10 @@
 
 http://cdn.gea.esac.esa.int/Gaia/gdr2/sso_observation/csv/
 
+_or_ Gaia-DR3 observations from somewhat differently formatted files at
+
+http://cdn.gea.esac.esa.int/Gaia/gdr3/Solar_system/sso_observation/
+
    into the ADES format.  Run as,  for example,
 
 ./csv2ades input_file.csv > output.ades
@@ -10,16 +14,18 @@ http://cdn.gea.esac.esa.int/Gaia/gdr2/sso_observation/csv/
    The second example would output only observations for object (4179)
 Toutatis.  The first would output all of the observations.
 
-   Further notes : there are four GZIPped files in the above directory,
-ranging from about 50 to 78 MBytes compressed and expanding to 150 to
-230 MBytes uncompressed.  Each contains astrometry for a specific range
-of numbered minor planets;  for example,  the first has data for objects
-(8) Flora to (4435) Holt.
+   Further notes : there are four GZIPped files in the above directory
+for DR2, ranging from about 50 to 78 MBytes compressed and expanding
+to 150 to 230 MBytes uncompressed.  Each contains astrometry for a
+specific range of numbered minor planets;  for example,  the first has
+data for objects (8) Flora to (4435) Holt.
 
 SsoObservation_-4284967216_-4284922946.csv.gz          8 - 4435
 SsoObservation_-4284922936_-4284857966.csv.gz       4436 - 10933
 SsoObservation_-4284857946_-4284702156.csv.gz      10935 - 26514
 SsoObservation_-4284702096_-4283326086.csv.gz      26520 - 164121
+
+   The Gaia DR3 data are less obviously arranged and are larger.
 */
 
 #include <stdio.h>
@@ -30,10 +36,17 @@ SsoObservation_-4284702096_-4283326086.csv.gz      26520 - 164121
 #include "watdefs.h"
 #include "date.h"
 
-static char *get_csv( char *obuff, const char *ibuff, unsigned nval)
+static char *get_csv( char *obuff, const char *ibuff,
+            const char *header, const char *tag)
 {
-   size_t i;
+   ssize_t i;
+   unsigned nval = 0;
+   const char *found = strstr( header, tag);
 
+   assert( found);
+   for( i = 0; i < found - header; i++)
+      if( header[i] == ',')
+         nval++;
    while( nval-- && ibuff)
       {
       ibuff = strchr( ibuff, ',');
@@ -47,12 +60,13 @@ static char *get_csv( char *obuff, const char *ibuff, unsigned nval)
    return( obuff);
 }
 
-static void csv_to_ades( const char *ibuff, const int low_num, const int high_num)
+static void csv_to_ades( const char *ibuff, const char *header,
+                          const int low_num, const int high_num)
 {
    char tbuff[90];
    int num;
 
-   get_csv( tbuff, ibuff, 3);
+   get_csv( tbuff, ibuff, header, "number_mp,");
    num = atoi( tbuff);
    if( num >= low_num && num <= high_num)
       {
@@ -65,28 +79,28 @@ static void csv_to_ades( const char *ibuff, const int low_num, const int high_nu
       printf( "    <stn>258</stn>\n");
       printf( "    <sys>ICRF_AU</sys>\n");
       printf( "    <ctr>0</ctr>\n");
-      printf( "    <pos1>%s</pos1>\n", get_csv( tbuff, ibuff, 18));
-      printf( "    <pos2>%s</pos2>\n", get_csv( tbuff, ibuff, 19));
-      printf( "    <pos3>%s</pos3>\n", get_csv( tbuff, ibuff, 20));
-      jd_obs = atof( get_csv( tbuff, ibuff, 6)) + jd_epoch;
+      printf( "    <pos1>%s</pos1>\n", get_csv( tbuff, ibuff, header, "x_gaia,"));
+      printf( "    <pos2>%s</pos2>\n", get_csv( tbuff, ibuff, header, "y_gaia,"));
+      printf( "    <pos3>%s</pos3>\n", get_csv( tbuff, ibuff, header, "z_gaia,"));
+      jd_obs = atof( get_csv( tbuff, ibuff, header, "epoch_utc,")) + jd_epoch;
       full_ctime( tbuff, jd_obs, FULL_CTIME_MILLISECS | FULL_CTIME_YMD
                            | FULL_CTIME_LEADING_ZEROES | FULL_CTIME_MONTHS_AS_DIGITS);
       tbuff[4] = tbuff[7] = '-';
       tbuff[10] = 'T';
       printf( "    <obsTime>%s</obsTime>\n", tbuff);
-      printf( "    <ra>%s</ra>\n",           get_csv( tbuff, ibuff, 7));
-      printf( "    <dec>%s</dec>\n",         get_csv( tbuff, ibuff, 8));
-      printf( "    <rmsRA>%f</rmsRA>\n",     atof( get_csv( tbuff, ibuff, 12)) / 1000.);
-      printf( "    <rmsDec>%f</rmsDec>\n",   atof( get_csv( tbuff, ibuff, 13)) / 1000.);
-      printf( "    <rmsCorr>%s</rmsCorr>\n", get_csv( tbuff, ibuff, 14));
-      get_csv( tbuff, ibuff, 15);
+      printf( "    <ra>%s</ra>\n",           get_csv( tbuff, ibuff, header, "ra,"));
+      printf( "    <dec>%s</dec>\n",         get_csv( tbuff, ibuff, header, "dec,"));
+      printf( "    <rmsRA>%f</rmsRA>\n",     atof( get_csv( tbuff, ibuff, header, "ra_error_random,")) / 1000.);
+      printf( "    <rmsDec>%f</rmsDec>\n",   atof( get_csv( tbuff, ibuff, header, "dec_error_random,")) / 1000.);
+      printf( "    <rmsCorr>%s</rmsCorr>\n", get_csv( tbuff, ibuff, header, "ra_dec_correlation_random,"));
+      get_csv( tbuff, ibuff, header, "g_mag,");
       if( *tbuff)
          {
          double g_flux, g_flux_sigma;
 
          printf( "    <mag>%s</mag>\n", tbuff);
-         g_flux       = atof( get_csv( tbuff, ibuff, 16));
-         g_flux_sigma = atof( get_csv( tbuff, ibuff, 17));
+         g_flux       = atof( get_csv( tbuff, ibuff, header, "g_flux"));
+         g_flux_sigma = atof( get_csv( tbuff, ibuff, header, "g_flux_error"));
          printf( "    <band>G</band>\n");
          printf( "    <rmsMag>%.3f</rmsMag>\n",
                            (g_flux_sigma / g_flux) / log( 2.512));
@@ -114,11 +128,17 @@ int main( const int argc, const char **argv)
          if( argv[i][0] != '-')
             {
             FILE *ifile = fopen( argv[i], "rb");
+            char header[800];
 
             assert( ifile);
-            if( fgets( buff, sizeof( buff), ifile))
-               while( fgets( buff, sizeof( buff), ifile))
-                  csv_to_ades( buff, low_num, high_num);
+            *header = '\0';
+            while( fgets( buff, sizeof( buff), ifile))
+               {
+               if( !memcmp( buff, "solution_id", 11))
+                  strcpy( header, buff);
+               else if( *buff != '#')
+                  csv_to_ades( buff, header, low_num, high_num);
+               }
             fclose( ifile);
             }
    fclose( hdr_file);
