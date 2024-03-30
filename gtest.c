cd15bbb5b1d01b27ee256e5a32c663bee4f80009
Bill-Gray <pluto@projectpluto.com>
2024-03-30 18:17:35 -0400

    'gtest' for various algorithms to compute GST : added the ThetaG function from SDP4/SGP4 (q.v.),  and slightly modified the 'lunatic' algorithm.

--- gtest.c
+++ gtest.c
@@ -43,10 +43,10 @@ Mathematically,  (3b) produces the same result as (1),  but with fewer
 excess rotations.  (The value is roughly 365.25 times smaller as jd
 tends to +/- inf.)  So the roundoff error is drastically decreased.
 
-   (I should perhaps mention that Meeus did a lot of work in an era
-where floating-point math was often done with 32-bit quantities,
-giving about six digits of precision.  Which made tricks of this sort
-especially important.)
+   (Meeus did a lot of work in an era where floating-point math was
+often done with 32-bit quantities, giving about seven digits of
+precision.  Simply using (1) would have led to large errors almost
+immediately.)
 
    But for large/small enough values of jd,  excess * (jd - 2451545.0)
 can still be a large number.  So I decided to try to make use of the
@@ -73,12 +73,15 @@ small enough that we handle multiples of 365 days with no risk
 of large numbers occurring.  So,  essentially a two-step variant
 of green_sidereal_time_better( ).
 
-   The whole point is somewhat theoretical here.  Our knowledge of the
+   The whole point is somewhat theoretical here.  A bit of testing
+with this code shows that the roundoff error for the 'simple' version
+is under an arcsecond after six million years.  Our knowledge of the
 earth's rotation,  and its unpredictable irregular nature,  are greater
-sources of error.  But it _would_ be relevant in similar situations
-where lots of rotations accumulate and one then wants to know the
-remaining angle.  Or any linear function where your interest is in a
-fractional remainder. */
+sources of error by several orders of magnitude.  But it _would_ be
+relevant in similar situations where lots of rotations accumulate and
+one then wants to know the remaining angle.  Or any linear function
+where your interest is in a fractional remainder.  (And such tricks
+were almost obligatory back in the days of 32-bit floats.)  */
 
 const double pi =
    3.1415926535897932384626433832795028841971693993751058209749445923;
@@ -154,6 +157,8 @@ double green_sidereal_time_lunatic( double jd_ut)
    long double t_cen, rval, base_t;
    long idays, n;
    const long double excess_365 = -0.238711305488250057038558065252110426648414328085786011;
+   const double excess_1461 =
+      0.030802144333333333333333333333333333333333333333333333333333333333;
    const long double excess_46751 = 0.0000212523803331051791010723248916267396760209901868486;
 
    jd_ut -= 2451545.0;        /* set relative to 2000.0 */
@@ -165,6 +170,10 @@ double green_sidereal_time_lunatic( double jd_ut)
    n = idays / 46751;
    rval = omega0 + n * excess_46751;
    idays -= n * 46751;
+
+   n = idays / 1461;
+   rval += n * excess_1461;
+   idays -= n * 1461;
 
    n = idays / 365;
    rval += n * excess_365;
@@ -186,6 +195,38 @@ static double fix_ang( double ang)
    return( ang);
 }
 
+/* thetag: computes Greenwich sidereal time,  as an angle in
+radians from 0 to 2*pi,  for a given UT0 JD.  This is used for
+artsat computations (the SGP4/SDP4 theory).   I added it to see if
+this slightly different expression was a suitable match to the
+others. Between 1900 and 2100,  they agree to within better than
+0.1 milliarcsec. That's (much) better than we can expect the
+position of any artsat to be known.    */
+
+static inline double ThetaG( const double jd)
+{
+//const double omega_E = excess / 360. + 1.;
+  const double omega_E = 1.00273790934;
+                 /* Reference:  The 1992 Astronomical Almanac, page B6. */
+                 /* Earth rotations per sidereal day (non-constant) */
+  const double UT = fmod( jd + .5, 1.);
+  const double seconds_per_day = 86400.;
+  const double jd_2000 = 2451545.0;   /* 1.5 Jan 2000 = JD 2451545. */
+  double t_cen, GMST, rval;
+
+//t_cen = (jd      - jd_2000) / 36525.;
+  t_cen = (jd - UT - jd_2000) / 36525.;
+  GMST = 24110.54841 + t_cen * (8640184.812866 + t_cen *
+                           (0.093104 - t_cen * 6.2E-6));
+  GMST = GMST / seconds_per_day + omega_E * UT;
+//GMST = fmod( GMST / seconds_per_day + omega_E * UT, 1.);
+  if( GMST < 0.)
+     GMST += 1.;
+  rval = 2. * pi * GMST;
+
+  return( rval);
+} /*Function thetag*/
+
 int main( const int argc, const char **argv)
 {
    if( argc == 2)
@@ -195,11 +236,13 @@ int main( const int argc, const char **argv)
       double v2 = green_sidereal_time_better( jd) * 180. / pi;
       double v3 = green_sidereal_time_best( jd) * 180. / pi;
       double v4 = green_sidereal_time_lunatic( jd) * 180. / pi;
+      double tg = ThetaG( jd) * 180. / pi;
 
       printf( "v1 %30.15f %25.15f (simple)\n", v1, fix_ang( v1));
       printf( "v2 %30.15f %25.15f (Meeus) \n", v2, fix_ang( v2));
       printf( "v3 %30.15f %25.15f ('best')\n", v3, fix_ang( v3));
       printf( "v4 %30.15f %25.15f ('lunatic')\n", v4, fix_ang( v4));
+      printf( "tg %30.15f %25.15f (ThetaG)\n", tg, fix_ang( tg));
       }
    else
       printf( "'gtest' takes a JD as a command line argument and computes\n"
