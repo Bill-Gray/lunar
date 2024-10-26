11bc85d9c703b815a6535bc9b4455c588cde7832
Bill-Gray <pluto@projectpluto.com>
2024-10-25 21:25:26 -0400

    Modified previous commit ac99be3120b8b to avoid use of INTnn_MAX and similar #defines.  The availability of such #defines is standard-dependent;  the revised code avoids this,  is less complicated,  and is really the way I should have done things to begin with...

--- mjd_defs.h
+++ mjd_defs.h
@@ -1,30 +1,20 @@
 #ifndef MJD_DEFS_H
 #define MJD_DEFS_H
 
-#include <stdint.h>
-#include <limits.h>
-
 /* These macros determine the MJD of the start of a month in the given
-'YEAR' at compile time.  If sizeof( long) == 8,  they are valid for
-(Gregorian) years after roughly 20 billion years ago. This was chosen
-to comfortably include all dates since the Big Bang.  If sizeof( long)
-== 4,  they only work between years -5877630 and +5881469;  beyond
-that,  the result overflows the 32-bit long.
+(Gregorian) 'YEAR' at compile time.  Separate macros are needed for
+positive and non-negative years.
 
    February 1 will always be 31 days after January 1.  March 1 through
 December 1 will always be a fixed number of days before January 1 of
 the following year.   */
 
-#if LONG_MAX == INT64_MAX
-   #define BASE_YEAR 19999999999L
-#elif LONG_MAX == INT32_MAX
-   #define BASE_YEAR 1999999999L
-#else
-   #error "Long integers have an unrecognized size"
-#endif
-#define JAN_1( YEAR) (((YEAR) * 365L + ((YEAR) + BASE_YEAR) / 4L - ((YEAR) + BASE_YEAR) / 100L \
-                         + ((YEAR) + BASE_YEAR) / 400L) - 678940L \
-                         - ((BASE_YEAR + 1L) / 400L) * 97L)
+#define JAN_1_POS( YEAR) ((YEAR) * 365L + ((YEAR) - 1) / 4L - ((YEAR) - 1) / 100L \
+                                        + ((YEAR) - 1) / 400L - 678940L)
+#define JAN_1_NEG( YEAR) ((YEAR) * 365L + (YEAR) / 4L - (YEAR) / 100L \
+                                        + (YEAR) / 400L - 678940L - 1L)
+#define JAN_1( YEAR)  ((YEAR) > 0 ? JAN_1_POS( YEAR) : JAN_1_NEG( YEAR))
+
 #define FEB_1( YEAR) (JAN_1( YEAR) + 31)
 #define DEC_1( YEAR) (JAN_1( (YEAR)+1) - 31)
 #define NOV_1( YEAR) (DEC_1( YEAR) - 30)
